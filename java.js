a8d768a238d1a76db89856844aee00786058f680
Josekar24 <johndoe@example.com>
2024-02-16 13:17:39 +0100

    José

--- java.js
+++ java.js
@@ -1,8 +1,8 @@
 "use strict";
 
-let nombre = confirm("Eres el jefe?", false);
-if(nombre== true){
+let nombre1 = confirm("Eres el jefe?", false);
+if(nombre1== true){
     alert("Eres el jefe")
-}else if(nombre== false){
+}else if(nombre1== false){
     alert("No eres el jefe")
 };
